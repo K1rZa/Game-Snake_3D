3d763add225c8c225a9bd61fa554b9a820c610d2
K1rZa <kirill.white7335@gmail.com>
2024-03-03 18:20:21 +0400

    modified:   JS/game_code.js

--- JS/game_code.js
+++ JS/game_code.js
@@ -5,44 +5,54 @@ const createScene = function () {
 	const scene = new BABYLON.Scene(engine)
 	scene.clearColor = new BABYLON.Color3.Black()
 
-	var camera = new BABYLON.FreeCamera(
-		'camera1',
-		new BABYLON.Vector3(0, 1300, -900),
+	var camera = new BABYLON.ArcRotateCamera(
+		'camera',
+		0,
+		0,
+		10,
+		new BABYLON.Vector3(0, 600, -800),
 		scene
 	)
+
+	camera.wheelDeltaPercentage = 0.1
 
 	camera.setTarget(BABYLON.Vector3.Zero())
 
 	camera.attachControl(canvas, true)
 
-	var light = new BABYLON.HemisphericLight(
+	var light = new BABYLON.DirectionalLight(
 		'light',
-		new BABYLON.Vector3(0, 1, 0),
+		new BABYLON.Vector3(-100, -500, 250),
 		scene
 	)
 
-	light.intensity = 0.7
+	light.intensity = 0.8
 
-    const ground_material = new BABYLON.StandardMaterial("ground_material", scene)
-    ground_material.diffuseColor = new BABYLON.Color3(0, 0.75, 0)
+	const ground_material = new BABYLON.StandardMaterial('ground_material', scene)
+	ground_material.diffuseColor = new BABYLON.Color3(0, 0.75, 0)
 
 	var ground = BABYLON.MeshBuilder.CreateGround(
 		'ground',
-		{ width: 1152, height: 1152 },
+		{ width: 672, height: 672 },
 		scene
 	)
-    ground.material = ground_material
+	ground.receiveShadows = true
+	ground.material = ground_material
 
 	const box_material = new BABYLON.StandardMaterial('box_material', scene)
 	box_material.diffuseColor = new BABYLON.Color3(1, 1, 1)
 
 	var box = BABYLON.MeshBuilder.CreateBox(
 		'box',
-		{ width: 64, height: 64, depth: 64 },
+		{ width: 32, height: 32, depth: 32 },
 		scene
 	)
 	box.material = box_material
-	box.position.y = 32
+	box.position.y = 16
+
+	var shadow = new BABYLON.ShadowGenerator(512, light)
+	shadow.usePoissonSampling = true
+	shadow.getShadowMap().renderList.push(box)
 
 	return scene
 }
